821f661d0a246b23de9c9d1bfb5bbd26d8aa2f86
Philip Whitehouse <philip@whiuk.com>
2019-08-10 20:58:45 +0100

    Add feedback when nothing happens

--- src/main/java/com/whiuk/philip/worldquest/ItemsTab.java
+++ src/main/java/com/whiuk/philip/worldquest/ItemsTab.java
@@ -57,7 +57,10 @@ class ItemsTab extends SidebarTab {
     public void onClick(MouseEvent e) {
         for (int i = 0; i < game.player.inventory.size(); i++) {
             if (inventoryButtonLocation(i, 0).contains(e.getPoint())) {
-                game.useItem(i);
+                boolean actionPerformed = game.useItem(i);
+                if (!actionPerformed) {
+                    game.eventMessage("Nothing happens");
+                }
                 return;
             } else if (inventoryButtonLocation(i, 1).contains(e.getPoint())) {
                 game.equipItem(i);
