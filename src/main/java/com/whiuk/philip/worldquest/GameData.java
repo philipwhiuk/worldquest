47f23b0844097419660930993b5f909d65e82b4f
Philip Whitehouse <philip@whiuk.com>
2019-08-16 00:39:35 +0100

    Load recipes from Scenario

--- src/main/java/com/whiuk/philip/worldquest/GameData.java
+++ src/main/java/com/whiuk/philip/worldquest/GameData.java
@@ -356,6 +356,12 @@ public class GameData {
                 game.showCrafting(new CraftingOptions("Smithing", recipeList.get("Smith")));
             }
         });
+        itemActions.put("Fish", new ItemAction() {
+            @Override
+            void perform(WorldQuest game, Tile tile, Player player, int bar, int na) {
+                game.attemptResourceGathering(resourceGathering.get("Fish"), tile);
+            }
+        });
     }
 
     class ResourceGathering {
@@ -403,25 +409,53 @@ public class GameData {
         }
     }
 
-    private void loadRecipes(BufferedReader buffer) {
-        Recipe Bronze = new Recipe(
-                Arrays.asList(
-                        Recipe.RecipeItem.one(items.get("CopperOre")),
-                        Recipe.RecipeItem.one(items.get("TinOre"))),
-                Arrays.asList(Recipe.RecipeItem.one(items.get("BronzeBar"))),
-                "Bronze Bar",
-                100,
-                Collections.emptyMap(),
-                Collections.singletonMap("Smelting", 10));
-        recipeList.put("Smelt", Arrays.asList(Bronze));
-        Recipe BronzeSword = new Recipe(
-                Arrays.asList(new Recipe.RecipeItem(items.get("BronzeBar"), 2)),
-                Arrays.asList(Recipe.RecipeItem.one(items.get("BronzeSword")),
-                "Bronze Sword",
-                100,
-                Collections.emptyMap(),
-                Collections.singletonMap("Smithing", 10));
-        recipeList.put("Smith", Arrays.asList(BronzeSword));
+    private void loadRecipes(BufferedReader buffer) throws IOException {
+        int recipesCount = Integer.parseInt(buffer.readLine());
+        Map<String,Recipe> allRecipes = new HashMap<>();
+        for (int r = 0; r < recipesCount; r++) {
+            String[] recipeData = buffer.readLine().split(",");
+            String recipeName = recipeData[0];
+            int inputItemCount = Integer.parseInt(recipeData[1]);
+            int outputItemCount = Integer.parseInt(recipeData[2]);
+            String outputName = recipeData[3];
+            int successChance = Integer.parseInt(recipeData[4]);
+            int skillRequirementCount = Integer.parseInt(recipeData[5]);
+            int experienceGainedCount = Integer.parseInt(recipeData[6]);
+            ArrayList<Recipe.RecipeItem> inputItems = new ArrayList<>(inputItemCount);
+            for (int i = 0; i < inputItemCount; i++) {
+                String[] inputItemData = buffer.readLine().split(",");
+                Recipe.RecipeItem recipeItem = new Recipe.RecipeItem(items.get(inputItemData[0]), Integer.parseInt(inputItemData[1]));
+                inputItems.add(recipeItem);
+            }
+            ArrayList<Recipe.RecipeItem> outputItems = new ArrayList<>(outputItemCount);
+            for (int i = 0; i < outputItemCount; i++) {
+                String[] outputItemData = buffer.readLine().split(",");
+                Recipe.RecipeItem recipeItem = new Recipe.RecipeItem(items.get(outputItemData[0]), Integer.parseInt(outputItemData[1]));
+                outputItems.add(recipeItem);
+            }
+            Map<String, Integer> skillRequirements = new HashMap<>(skillRequirementCount);
+            for (int i = 0; i < skillRequirementCount; i++) {
+                String[] skillRequirementData = buffer.readLine().split(",");
+                skillRequirements.put(skillRequirementData[0], Integer.parseInt(skillRequirementData[1]));
+            }
+            Map<String, Integer> experienceGained = new HashMap<>(experienceGainedCount);
+            for (int i = 0; i < experienceGainedCount; i++) {
+                String[] experienceGainData = buffer.readLine().split(",");
+                skillRequirements.put(experienceGainData[0], Integer.parseInt(experienceGainData[1]));
+            }
+            Recipe recipe = new Recipe(inputItems, outputItems, outputName, successChance, skillRequirements, experienceGained);
+            allRecipes.put(recipeName, recipe);
+        }
+        int recipeCollectionsCount = Integer.parseInt(buffer.readLine());
+        for (int rc = 0; rc < recipeCollectionsCount; rc++) {
+            String[] recipeCollectionData = buffer.readLine().split(",");
+            int collectionRecipesCount = Integer.parseInt(recipeCollectionData[1]);
+            List<Recipe> recipes = new ArrayList<>();
+            for (int r = 0; r < collectionRecipesCount; r++) {
+                recipes.add(allRecipes.get(buffer.readLine()));
+            }
+            recipeList.put(recipeCollectionData[0], recipes);
+        }
     }
 
     private void loadUses(BufferedReader buffer) {
